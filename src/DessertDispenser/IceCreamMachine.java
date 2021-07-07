15526b26661f845f8356939c8ceecb52096aac1a
Freddy Canido <freddy.canido12@gmail.com>
2021-07-06 21:12:14 -0400

    basics done

--- src/DessertDispenser/IceCreamMachine.java
+++ src/DessertDispenser/IceCreamMachine.java
@@ -31,43 +31,75 @@ public class IceCreamMachine {
         topping.amount = 25;
     }
 
-    public static void coneDispenser(Container container, int amount) {
-        if (amount > container.amount) {
+    public static void coneDispenser(Container container) {
+        if (1 <= container.amount) {
             if (container.coneDip.equalsIgnoreCase("none")) {
-                System.out.println();
+                System.out.println(container.containerType + " dispensed.");
+                container.amount--;
+
             } else {
-                System.out.println();
+                System.out.println(container.containerType + " with " + container.coneDip + " dip dispensed.");
+                container.amount--;
             }
         } else {
             System.out.println("Not enough cones, please refill.");
         }
-
-
     }
 
-    public static void leftLever(IceCream left, int scoops) {
-        if (scoops > left.scoops) {
-            System.out.println(left.scoops + " scoops of " + left.flavor + " ice cream dispensed.");
+    public static void toppingDispenser(Topping topping, int amount) {
+        if (amount <= topping.amount) {
+            if (amount == 1) {
+                System.out.println(amount + " spoon of " + topping.topping + " dispensed.");
+            } else {
+                System.out.println(amount + " spoons of " + topping.topping + " dispensed.");
+            }
+            topping.amount -= amount;
+        } else {
+            System.out.println("Not enough " + topping.topping + " toppings, please refill");
+        }
+    }
+
+    public static void leftLever(IceCream left, double scoops) {
+        if (scoops <= left.scoops) {
+            if (scoops == 1) {
+                System.out.println(scoops + " scoop of " + left.flavor + " ice cream dispensed.");
+            } else {
+                System.out.println(scoops + " scoops of " + left.flavor + " ice cream dispensed.");
+            }
+            left.scoops -= scoops;
         } else {
             System.out.println("Not enough " + left.flavor + " Ice Cream, please refill.");
         }
     }
 
-    public static void midLever(IceCream left, IceCream right, int scoops) {
-        if (scoops > (left.scoops / 2) + (right.scoops / 2)) {
-            System.out.println(scoops + " scoops of " + left.flavor + " and " + right.flavor + " swirl ice cream");
+    public static void midLever(IceCream left, IceCream right, double scoops) {
+        if (scoops <= (left.scoops / 2) + (right.scoops / 2)) {
+            if (scoops == 1) {
+                System.out.println(scoops + " scoop of " + left.flavor + " and " + right.flavor + " swirl ice cream");
+            } else {
+                System.out.println(scoops + " scoops of " + left.flavor + " and " + right.flavor + " swirl ice cream");
+            }
+            left.scoops -= scoops / 2;
+            right.scoops -= scoops / 2;
         } else {
             System.out.println("Not enough " + left.flavor + " and " + right.flavor + " Ice Cream, please refill.");
         }
     }
 
-    public static void rightLever(IceCream right, int scoops) {
-        if (scoops > right.scoops) {
-            System.out.println(right.scoops + " scoops of " + right.flavor + " ice cream dispensed.");
+    public static void rightLever(IceCream right, double scoops) {
+        if (scoops <= right.scoops) {
+            if (scoops == 1) {
+                System.out.println(scoops + " scoop of " + right.flavor + " ice cream dispensed.");
+            } else {
+                System.out.println(scoops + " scoops of " + right.flavor + " ice cream dispensed.");
+            }
+            right.scoops -= scoops;
         } else {
             System.out.println("Not enough " + right.flavor + " Ice Cream, please refill.");
         }
     }
 
-
+    public static void displayIceCream(int scoops, int cone) {
+        System.out.println("Your final order comes to");
+    }
 }
--- src/DessertDispenser/Main.java
+++ src/DessertDispenser/Main.java
@@ -6,5 +6,16 @@ public class Main {
         // write your code here
         IceCream left = new IceCream("chocolate", true);
         IceCream right = new IceCream("vanilla", true);
+        Topping oreo = new Topping("Oreo");
+        Topping sprinkles = new Topping("Sprinkles");
+        Container waffleCone = new Container("waffle cone", "none");
+        Container sugarCone = new Container("sugar cone", "chocolate");
+//        IceCreamMachine.fillLeft(left);
+//        IceCreamMachine.refillToppings(oreo);
+//        IceCreamMachine.refillContainers(waffleCone);
+        IceCreamMachine.coneDispenser(waffleCone);
+        IceCreamMachine.toppingDispenser(oreo, 2);
+        IceCreamMachine.leftLever(left, 1);
+
     }
 }
